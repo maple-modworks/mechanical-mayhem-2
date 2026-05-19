53f1c07c8a1a03c0272064f8657e58ff7f48820e
Herr Katze <scmcgowen@gmail.com>
2026-05-19 16:53:00 -0700

    Make bioethanol bottomless

--- pack/kubejs/server_scripts/tags/tags.js
+++ pack/kubejs/server_scripts/tags/tags.js
@@ -2,4 +2,6 @@ ServerEvents.tags("item", event => {
   event.add("c:music_discs", "supplementaries:pancake");
 })
 
-
+ServerEvents.tags("fluid", event => {
+  event.add("create:bottomless/allow", "createaddition:bioethanol")
+})
